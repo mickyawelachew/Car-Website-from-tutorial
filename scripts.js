05459786bf82e432663711de92ce08463c757901
Micky <mickyawelachew@yahoo.com>
2024-12-28 20:05:02 -0800

    Front page

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const menuIcon = document.getElementById("menu-icon");
+const navLinks =  document.getElementById("nav-links");
+
+menuIcon.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    menuIcon.classList.toggle('active');
+});
